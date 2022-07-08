0e7437a6a975651e694bbc014e489028a7af21ce
powzx <69313768+powzx@users.noreply.github.com>
2022-07-08 11:12:43 +0800

    Add signer implementation

---
+++ lib/signer.dart
@@ -0,0 +1,58 @@
+import 'package:blocksupply_flutter/storage_service.dart';
+import 'package:ecdsa/ecdsa.dart';
+import 'package:elliptic/elliptic.dart';
+
+Signer signer;
+
+Future<void> initSigner() async {
+  StorageService _storageService = new StorageService();
+  if (await _storageService.containsKeyInSecureData('blockchain_private_key')) {
+    signer = new Signer.fromExisting(
+        await _storageService.readSecureData('blockchain_private_key'));
+  } else {
+    signer = new Signer();
+  }
+}
+
+class Signer {
+  PublicKey _publicKey;
+  PrivateKey _privateKey;
+  bool hasSetUp;
+
+  final StorageService _storageService = new StorageService();
+
+  Signer() {
+    var curve = getSecp256k1();
+
+    _privateKey = curve.generatePrivateKey();
+    _publicKey = _privateKey.publicKey;
+
+    hasSetUp = false;
+
+    print('Created new public key: ${this.getPublicKeyHex()}');
+  }
+
+  Signer.fromExisting(String privateKey) {
+    var curve = getSecp256k1();
+
+    _privateKey = PrivateKey.fromHex(curve, privateKey);
+    _publicKey = _privateKey.publicKey;
+
+    hasSetUp = true;
+
+    print('Found public key: ${this.getPublicKeyHex()}');
+  }
+
+  void writeToSecureStorage() {
+    _storageService.writeSecureData(
+        new StorageItem('blockchain_private_key', _privateKey.toHex()));
+  }
+
+  String getPublicKeyHex() {
+    return _publicKey.toCompressedHex();
+  }
+
+  String sign(List<int> bytes) {
+    return deterministicSign(_privateKey, bytes).toCompactHex();
+  }
+}
